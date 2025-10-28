2a9dfc8e52b9c8c1138e4cd62540db681e69a6ed
Youhana <youhana939@gmail.com>
2025-10-28 14:08:02 +0300

    Initial commit: Appium Java TestNG setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# MulkiApp
+# appiumSetup
+
+Java + TestNG + Appium example.
+
+## Prerequisites
+- Java 11+ with JAVA_HOME
+- Android SDK with ANDROID_HOME and platform-tools on PATH
+- Appium Server running (2.x) and drivers installed (uiautomator2)
+- Maven 3.8+
+
+## Run
+1. Start Appium server
+2. Connect device/emulator
+3. mvn -Dtest=appiumSetup test
+
+## Notes
+- Device UDID and capabilities are set in tests; adapt for your environment.
--- src/test/java/appiumSetup.java
+++ src/test/java/appiumSetup.java
@@ -9,23 +9,24 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import pages.LoginPage;
 
-public class mulkiRun {
+public class appiumSetup {
 
     @Test
     public void openMulkiAPp() throws MalformedInputException, MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Mi A3");
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        caps.setCapability("appPackage", "com.mulkiapp");
-        caps.setCapability("appActivity", "com.mulkiapp.main.presentation.MainActivity");
+		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		caps.setCapability(MobileCapabilityType.UDID, "R58R91ZLQWT");
+		caps.setCapability("appPackage", "com.bluev.users.dev");
+		caps.setCapability("appActivity", "com.bluev.users.MainActivity");
         
-        AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4724/"), caps);
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), caps);
         
         // Create login page object
         LoginPage loginPage = new LoginPage(driver);
         
-        // Perform login actions
-        loginPage.clickGetStarted();
-        loginPage.login("test@example.com", "password123");
+		// Perform login actions
+		loginPage.completeLogin("test@example.com", "password123");
     }
 }
